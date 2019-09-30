867dbadf4a24b824b896848101fa56996e061408
Najeam <nmehanma@gmal.com>
2019-09-30 14:00:21 +0000

    edited

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,22 @@
 const request = require('request');
 
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
-  //console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+const fetchBreedDescription = (breed,callback) => request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+
   const data = JSON.parse(body);
   
-  if (data.length === 0) {
-    console.log("not found");
-  } else if (error) {
-    console.log(error);
+  if (error) {
+    callback(error, null);
+  } else if (data.length === 0) {
+    callback(null, "breed not found");
   } else {
+    callback(null, data[0].description.trim());
     
-    console.log((data[0].description));
-    // console.log(data;
-    console.log(typeof data); // Print the HTML for the Google homepage.
   }
-
-  
-  
+ 
 });
 
 
+
+module.exports = { fetchBreedDescription };
 
