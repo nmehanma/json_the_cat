af4c2e3eb02d51b341e2e5b5ec8e5560264bb38f
Najeam <nmehanma@gmal.com>
2019-09-30 13:58:57 +0000

    edited index file

---
+++ index.js
@@ -0,0 +1,15 @@
+//index.js
+
+const { fetchBreedDescription }  = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+const callback = (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+};
+
+fetchBreedDescription(breedName, callback);
